8f6a2b972d95f213b78865f3aa9ba206517991a6
Sorata <sorata.arctica@gmail.com>
2024-05-12 21:27:51 +0200

    PUTO patrón de PUTO diseño de PUTO iterador de PUTO contenedores

--- src/main/webapp/js/editar_ficha.js
+++ src/main/webapp/js/editar_ficha.js
@@ -23,6 +23,58 @@ document.addEventListener('DOMContentLoaded', function(){
         document.getElementById("alineamiento").value = datos.alineamiento;
         document.getElementById("px").value = datos.px;
         document.getElementById("bono_competencia").value = datos.bono_competencia;
+
+        var array_caract = datos.Caract;
+        var iter = new Iterator(array_caract);
+        for (var item = iter.first(); iter.hasNext(); item = iter.next()) {
+            switch (item.idCaract) {
+                case 1:
+                    document.getElementById("FUE").value = item.puntuacion;
+                    document.getElementById("mod_fuerza").value = item.modificador;
+                    break;
+                case 2:
+                    document.getElementById("DES").value = item.puntuacion;
+                    document.getElementById("mod_destreza").value = item.modificador;
+                    break;
+                case 3:
+                    document.getElementById("CON").value = item.puntuacion;
+                    document.getElementById("mod_constitucion").value = item.modificador;
+                    break;
+                case 4:
+                    document.getElementById("INT").value = item.puntuacion;
+                    document.getElementById("mod_inteligencia").value = item.modificador;
+                    break;
+                case 5:
+                    document.getElementById("SAB").value = item.puntuacion;
+                    document.getElementById("mod_sabiduria").value = item.modificador;
+                    break;
+                case 6:
+                    document.getElementById("CAR").value = item.puntuacion;
+                    document.getElementById("mod_carisma").value = item.modificador;
+                    break;
+            }
+        }
+    }
+
+    function Iterator (items){
+        this.index = 0;
+        this.items = items;
+    }
+
+    Iterator.prototype = {
+        first: function (){
+            this.reset();
+            return this.next();
+        },
+        next: function (){
+            return this.items[this.index++];
+        },
+        hasNext: function (){
+            return this.index <= this.items.length;
+        },
+        reset: function (){
+            this.index = 0;
+        }
     }
 
 
